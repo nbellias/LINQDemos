32700da82224c44cbfe90f04dce3716c4f1f92e4
Nikolaos Bellias <nmpellias@cognity.gr>
2022-07-19 19:34:33 +0300

    Add project files.

---
+++ LINQDemos.Aggregates/Program.cs
@@ -0,0 +1,18 @@
+﻿using LINQDemos.Shared.DataContext;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LINQDemos.Aggregates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new AdventureWorksContext();
+            var employees = context.Employees.ToList();
+
+            Console.WriteLine(employees.Count());
+            
+        }
+    }
+}
